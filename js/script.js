878d1df445d3d42ddbed033a25af10728b8b4265
Floriberto Hernandez Jr <fhjr10code@zoho.com>
2023-04-25 20:25:10 -0700

    movie details added

--- js/script.js
+++ js/script.js
@@ -74,6 +74,76 @@ const displayPopularShows = async () => {
   });
 };
 
+// Function will get movie id from url and fetch movie data from api
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+};
+
 // Function will fetch data from API
 const fetchAPIData = async (endpoint) => {
   const API_KEY = '2237c973fb5b3b0df94d16ffb2f91448'; // Put your api key here
@@ -112,6 +182,11 @@ function highlightActiveLink() {
   });
 }
 
+// Function will add cmmas to large numbers
+const addCommasToNumber = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -123,7 +198,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
